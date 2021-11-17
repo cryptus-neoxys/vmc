0e8a4913e7dbc91bb53615c87c1f8dfe848f838d
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-11-18 01:06:36 +0530

    ✨ signin working

--- signin_form.php
+++ signin_form.php
@@ -2,18 +2,18 @@
 include('includes/database.php');
 
 if (isset($_POST['submit'])) {
-	$username = $_POST['username'];
-	$password = $_POST['password']; {
-		$result = mysqli_query($con, "SELECT * FROM user WHERE username = '$username' and password='$password'");
+}
+$username = $_POST['username'];
+$password = $_POST['password'];
+$result = mysqli_query($con, "SELECT * FROM user WHERE username = '$username' and password='$password'");
 
-		$row = mysqli_fetch_array($result);
-		$count = mysqli_num_rows($result);
-		if ($count == 0) {
-			echo "<script>alert('Please check your username and password!'); window.location='signin.php'</script>";
-		} else if ($count > 0) {
-			session_start();
-			$_SESSION['id'] = $row['id'];
-			header("location:home.php");
-		}
-	}
+$row = mysqli_fetch_array($result);
+$count = mysqli_num_rows($result);
+if ($count == 0) {
+	echo "<script>alert('Please check your username and password!'); window.location='login.php'</script>";
+} else if ($count > 0) {
+	session_start();
+	$_SESSION['id'] = $row['id'];
+	echo "<script>alert('Logged in successfully!'); window.location='index.php'</script>";
+	// header("location:index.php");
 }
